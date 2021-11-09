5a99fd4838f6b2e5dc3cfa80e483cc3a494c3ca7
Lucas Batista <lucas.bt46@gmail.com>
2021-11-09 15:35:39 -0300

    Product

---
+++ src/main/java/com/punkmetal/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.punkmetal.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.punkmetal.course.entities.Product;
+import com.punkmetal.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	public List<Product> findAll() {
+		return productRepository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> op = productRepository.findById(id);
+		return op.get();
+	}
+}
